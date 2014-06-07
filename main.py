ae8ca0434f3a6654f36a7d8a94ea351307115f98
Bo Liu <6liubo8@gmail.com>
2014-06-07 15:58:23 -0700

    first python code to read and analyze the csv files

---
+++ main.py
@@ -0,0 +1,9 @@
+#Main file to extract features and analysis the csv files
+import analysis_file as ana
+
+
+if __name__ == '__main__':
+
+    filepath = '../Data/'
+    ana.analysis_file(filepath)
+
